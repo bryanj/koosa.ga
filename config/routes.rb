090b7d3bb808a061f8988c46da507dc7876c22bf
Jo Seunghyun <sky@izz.kr>
2018-03-11 15:13:28 +0900

    로그인 기능 추가

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  get "/login", to: "users#login"
+  post "/login", to: "users#login_proc"
 end
